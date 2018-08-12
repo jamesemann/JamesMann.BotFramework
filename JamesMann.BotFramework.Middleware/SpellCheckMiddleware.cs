b844977bf85ab1723545616cf9d20f9bc1282041
James Mann <james.mann@bcs.org>
2018-08-12 02:32:09 +0100

    initial commit

---
+++ JamesMann.BotFramework.Middleware/SpellCheckMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using JamesMann.BotFramework.Middleware.Extensions;
+
+namespace JamesMann.BotFramework.Middleware
+{
+    public class SpellCheckMiddleware :IMiddleware
+    {
+        public SpellCheckMiddleware(IConfiguration configuration)
+        {
+            ApiKey = configuration.GetValue<string>("SpellCheckKey");
+        }
+
+        public string ApiKey { get; }
+
+        public async Task OnTurn(ITurnContext context, MiddlewareSet.NextDelegate next)
+        {
+            context.Activity.Text = await context.Activity.Text.SpellCheck(ApiKey);
+
+            await next();
+        }
+    }
+}
---
+++ JamesMann.BotFramework.Middleware/TypingMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Bot.Builder;
+using JamesMann.BotFramework.Middleware.Extensions;
+using System.Threading.Tasks;
+
+namespace JamesMann.BotFramework.Middleware
+{
+    public class TypingMiddleware : IMiddleware
+    {
+        public async Task OnTurn(ITurnContext context, MiddlewareSet.NextDelegate next)
+        {
+            if (context.Activity.UserHasJustJoinedConversation() || context.Activity.UserHasJustSentMessage())
+            {
+                await context.Activity.DoWithTyping(async () =>
+                {
+                    await next();
+                });
+            }
+            else
+            {
+                await next();
+            }
+        }
+    }
+}
