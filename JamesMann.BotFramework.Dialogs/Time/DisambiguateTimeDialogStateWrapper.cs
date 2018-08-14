eef0f49e8f1ff889c14a590e607fd55f018945b7
James Mann <james.mann@bcs.org>
2018-08-14 16:52:34 +0100

    added dialog project

---
+++ JamesMann.BotFramework.Dialogs/Time/DisambiguateTimeDialogStateWrapper.cs
@@ -0,0 +1,40 @@
+﻿using JamesMann.BotFramework.Dialogs.Extensions;
+using System;
+using System.Collections.Generic;
+
+namespace JamesMann.BotFramework.Dialogs.Time
+{
+    // convenience helper to get/set dialog state
+    internal class DisambiguateTimeDialogStateWrapper
+    {
+        public DisambiguateTimeDialogStateWrapper(IDictionary<string,object> state)
+        {
+            State = state;
+        }
+
+        public IDictionary<string, object> State { get; }
+
+        public RecognizerExtensions.ResolutionList Resolutions {
+            get
+            {
+                return State["recognizedDateTime"] as RecognizerExtensions.ResolutionList;
+            }
+            set
+            {
+                State["recognizedDateTime"] = value;
+            }
+        }
+
+        public TimeSpan Time
+        {
+            get
+            {
+                return (TimeSpan)State["time"];
+            }
+            set
+            {
+                State["time"] = value;
+            }
+        }
+    }
+}
