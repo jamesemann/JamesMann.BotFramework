2b2bad60a4fde08d6f51bee22ca6699647541ea6
Arafat Tehsin <arafat@outlook.com>
2018-08-15 13:01:33 +0300

    Added the Sentiment Middlware to the project along with the ServiceCredentials for common CognitiveService calls.

---
+++ JamesMann.BotFramework.Middleware/SentimentMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using JamesMann.BotFramework.Middleware.Extensions;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamesMann.BotFramework.Middleware
+{
+    public class SentimentMiddleware : IMiddleware
+    {
+        public SentimentMiddleware(IConfiguration configuration)
+        {
+            ApiKey = configuration.GetValue<string>("SemanticsKey");
+        }
+
+        public string ApiKey { get; }
+
+        public async Task OnTurn(ITurnContext context, MiddlewareSet.NextDelegate next)
+        {
+            if (context.Activity.Type is ActivityTypes.Message)
+            {
+                context.Services.Add<string>(await context.Activity.Text.Sentiment(ApiKey));
+            }
+            
+            await next();
+        }
+
+    }
+}
